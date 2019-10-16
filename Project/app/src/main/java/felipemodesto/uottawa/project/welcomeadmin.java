9c27df3b9b56f563c8978f0c71533aee2baeab6d
吴浩哲 Wu Haozhe <48733023+HaozheWu@users.noreply.github.com>
2019-10-16 17:33:19 -0400

    Re Submit to the correct place with Correct File type

---
+++ Project/app/src/main/java/felipemodesto/uottawa/project/welcomeadmin.java
@@ -0,0 +1,19 @@
+package felipemodesto.uottawa.project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class welcomeadmin extends AppCompatActivity {
+    private TextView UserNameTextView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcomeadmin);
+        UserNameTextView=(TextView)findViewById(R.id.textView2);
+        UserNameTextView.setText(MainActivity.currentusername);
+    }
+
+
+}
