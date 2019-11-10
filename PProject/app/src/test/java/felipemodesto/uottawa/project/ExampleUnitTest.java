61eba7e2a2c4ae334a6e16fdc5fca8b7759b1e12
Skye <55254825+langqing8@users.noreply.github.com>
2019-11-09 21:20:53 -0500

    add three tests

--- PProject/app/src/test/java/felipemodesto/uottawa/project/ExampleUnitTest.java
+++ PProject/app/src/test/java/felipemodesto/uottawa/project/ExampleUnitTest.java
@@ -14,4 +14,36 @@ public class ExampleUnitTest {
     public void addition_isCorrect() {
         assertEquals(4, 2 + 2);
     }
+    
+    @Test
+    public void isEmail() {
+        String eamilStr = "00000@.com";
+        String eamilStr1 = "00000@qq.com";
+        System.out.println("email is valid: ===" + RegTool.isEmail(eamilStr));
+        System.out.println("email is valid: ===" + RegTool.isEmail(eamilStr1));
+    }
+
+    /**
+     * md5
+     *
+     */
+    @Test
+    public void isCorrectMD5() {
+        String pwd = "123456";
+        String pwdMD5Str32 = "e10adc3949ba59abbe56e057f20f883e";//123456md5  32passwordEncryption
+        String pwdMD5Str16 = "49ba59abbe56e057";//123456md5  16passwordEncryption
+        passwordEncryption passwordEncryption = new passwordEncryption();
+        assertEquals(pwdMD5Str32, passwordEncryption.passwordEncryption(pwd));
+        assertEquals(pwdMD5Str16, passwordEncryption.passwordEncryption(pwd));
+    }
+
+    /**
+     * price is number or not
+     *
+     */
+    @Test
+    public void isNumberPrice() {
+        String price = "000.09";
+        System.out.println("price is number==" + isNumeric(price));
+    }
 }
